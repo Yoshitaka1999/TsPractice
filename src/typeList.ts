f93d21e4efa3ec2002fc3b82e4b71fe0b8d4d872
Yoshitaka Kato <83825593+Yoshitaka1999@users.noreply.github.com>
2022-04-20 16:26:54 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "A";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+let tuple: [number, string] = [0, "A"];
+
+let any1: any = false;
+
+const func1 = (): void => {
+  const test = "test";
+};
+
+let null1: null = null;
+
+let undefind1: undefined = undefined;
+
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "aaa" };
